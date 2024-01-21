7bee87462d8661a4f77d9a79a1aff6d4ad68f39e
syedali237 <syedaliulhasan19@gmail.com>
2024-01-22 05:20:26 +0530

    HouseRobber

--- JS-solutions/0198_HouseRobber/sol.js
+++ JS-solutions/0198_HouseRobber/sol.js
@@ -10,9 +10,24 @@ var rob = function(nums) {
     return Math.max(rob, norob);
 };
 
-// nums = [1,2,3,1];
+nums = [1,2,3,1];
 // nums = [2,7,9,3,1];
 // nums = [1,3,1];
-nums = [2,1,1,2];
+// nums = [2,1,1,2];
 // nums = [1, 2];
-console.log(rob(nums));
+
+function rob2(nums){
+    if (nums.length < 2){
+        return nums[0];
+    }
+    let output = [];
+    output[0] = nums[0];
+    output[1] = Math.max(nums[0], nums[1]);
+
+    for (let i=2;i < nums.length; i++){
+        output.push(Math.max(output[i-2] + nums[i], output[i-1]));
+    }
+    console.log(output);
+    return output[output.length-1];
+}
+console.log(rob2(nums));
