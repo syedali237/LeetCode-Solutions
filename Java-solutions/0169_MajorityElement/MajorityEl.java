1310fafa7855cec70c1947bda1b1b020dee8b705
syedali237 <syedaliulhasan19@gmail.com>
2024-02-13 00:53:20 +0530

    MajorityElement

---
+++ Java-solutions/0169_MajorityElement/MajorityEl.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class MajorityEl {
+    static int majorityElement(int[] nums){
+        Arrays.sort(nums);
+        int n = nums.length;
+        return nums[n/2];
+    }
+    public static void main(String[] args) {
+        int[] nums = {2,2,1,1,1,2,2};
+        System.out.println(majorityElement(nums));
+    }
+}
