3438ac5e63e817bc67ca5377df9436e3a7cfb6fb
syedali237 <syedaliulhasan19@gmail.com>
2024-02-14 02:22:05 +0530

    renamed

--- Java-solutions/0169_MajorityElement/MajorityEl.java
+++ Java-solutions/0169_MajorityElement/MajorityEl.java
@@ -6,6 +6,24 @@ public class MajorityEl {
         int n = nums.length;
         return nums[n/2];
     }
+
+    public int majorityEl(int[] nums) {
+        int count = 0;
+        int element = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                element = num;
+                count = 1;
+            } else if (element == num) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return element;
+    }
     public static void main(String[] args) {
         int[] nums = {2,2,1,1,1,2,2};
         System.out.println(majorityElement(nums));
