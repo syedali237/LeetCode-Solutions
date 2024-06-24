2300b2decd11e07fb3d16870b2271a6e3f14656d
syedali237 <syedaliulhasan19@gmail.com>
2024-06-24 19:40:38 +0530

    updated solution

--- Java-solutions/0832_FlipImage/Flip.java
+++ Java-solutions/0832_FlipImage/Flip.java
@@ -5,8 +5,21 @@ public class Flip {
     public static void main(String[] args) {
         
     }
-
+    
     static int[][] flipAndInvertImage(int[][] image) {
+        for (int[] row : image){
+            // reverse this array
+            for (int i = 0; i < (image[0].length +1)/2; i++) {
+                // swap
+                int temp = row[i] ^ 1; // xor with 1
+                row[i] = row[image[0].length - i -1] ^ 1;
+                row[image[0].length - i -1] = temp;
+            }
+        }
+        return image;
+    }
+
+    static int[][] flipAndInvertImage2(int[][] image) { // using extra space
         int [][] rev = new int[image[0].length][image.length];
         for(int i=0; i<image.length; i++){
             int k=0;
